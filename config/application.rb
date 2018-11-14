05b61a1cbfcb94ed4e5753e95985247cf45ecffc
Nana Adjei Manu <claeusdev@gmail.com>
2018-11-13 19:07:11 -0500

    more cors goodies

--- config/application.rb
+++ config/application.rb
@@ -32,11 +32,11 @@ module IdeaboardApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'https://frosty-goodall-60b3cd.netlify.com/'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins 'https://frosty-goodall-60b3cd.netlify.com/'
+    #     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
+    #   end
+    # end
   end
 end
